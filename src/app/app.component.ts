6def69343e5aa885a11efe801c0af744f493e3da
mgriffoul <mathieu.griffoul@zenika.com>
2023-12-11 10:27:30 +0100

    g2sp init tailwind css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {Component} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'g2sp-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
