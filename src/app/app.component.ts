552182b08876ada1179974d72f827a2ee4f1f9ad
mgriffoul <mathieu.griffoul@zenika.com>
2023-12-13 17:33:23 +0100

    Create header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {NgOptimizedImage} from "@angular/common";
+import {HeaderComponent} from "./header/header.component";
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HeaderComponent],
   selector: 'g2sp-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Route } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {WorkInProgressComponent} from "./work-in-progress/work-in-progress.component";
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'home',
+    component: WorkInProgressComponent
+  },
+  {
+    path: 'beta-version',
+    component: HomeComponent
+  },
+  {path: '**', redirectTo: '/home'}
+];
