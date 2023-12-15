b5b84c409f46b39e6c4ae414a5d146836d265a45
mgriffoul <mathieu.griffoul@zenika.com>
2023-12-15 15:15:57 +0100

    First draw services page

--- src/app/page/temp-app/services/services.component.ts
+++ src/app/page/temp-app/services/services.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+
+export interface Sections {
+    iconName: string;
+    description: string;
+}
 
 @Component({
     selector: 'g2sp-services',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, MatIconModule],
     templateUrl: './services.component.html',
     styleUrl: './services.component.scss',
 })
-export class ServicesComponent {}
+export class ServicesComponent implements OnInit {
+    sections: Sections[] = [];
+
+    ngOnInit() {
+        this.initWhySections();
+    }
+
+    private initWhySections() {
+        this.sections = [
+            {
+                iconName: 'language',
+                description:
+                    'Des collaborateurs de plus en plus nombreux à utiliser massivement des outils informatiques',
+            },
+            {
+                iconName: 'psychology',
+                description:
+                    'Une culture souvent fragile de la sécurité informatique de nombreux collaborateurs',
+            },
+            {
+                iconName: 'bug_report',
+                description:
+                    'Des menaces cyber multiformes et croissantes, qui ciblent données, opérations et réputation',
+            },
+            {
+                iconName: 'lock_open',
+                description:
+                    'Un niveau de préparation incertain et hétérogène aux défaillances de vos systèmes critiques',
+            },
+        ];
+    }
+}
